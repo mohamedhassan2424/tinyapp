83c1c8516a448e40a4ba9d58428d27c5ec0ede3a
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-05 18:19:44 +0000

    committing code

--- express_server.js
+++ express_server.js
@@ -6,10 +6,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.set("view engine","ejs");
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls" , (req,res) => {
+    const templateVars= {urls : urlDatabase};
+    res.render("urls_index", templateVars)
+});
+
+app.get("/urls/:id" , (req,res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase };
+    res.render("urls_show",templateVars)
+})
 app.get("/hello", (req,res) => {
 res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
