c3a7ef2eaa1a4f67cd8f2de80f3aa05cf22930d0
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-06 17:55:06 +0000

    comitting code

--- express_server.js
+++ express_server.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const shortUrl = function generateRandomString(stringSize ){
+
+    let optionalChracters='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let inputString="";
+    for(let i=0 ; i < stringSize ; i++){
+        inputString += optionalChracters.charAt(Math.floor(Math.random()*optionalChracters.length));
+        console.log(inputString)
+    }
+    //console.log(inputString)
+    return inputString;
+
+};
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  
+
 };
-function generateRandomString(){
-    
-}
+
 app.set("view engine","ejs");
 app.use(express.urlencoded({ extended: true}));
 app.get("/", (req, res) => {
@@ -20,16 +32,31 @@ app.get("/urls" , (req,res) => {
 });
 app.get("/urls/new" , (req,res) => {
     res.render("urls_new")
+
 });
 app.post("/urls" , (req,res) => {
+    const id= Math.random().toString(36).substring(2,8)
     console.log(req.body);
-    res.send("Okay Recieved");
+    console.log(req.params.id)
+    const longUrlValue  = req.body.longURL
+    console.log(longUrlValue);
+    
+    urlDatabase[id] = longUrlValue;
+    //res.send("Okay Recieved");
+    res.redirect(`/urls/${id}`)
+
 });
 
 app.get("/urls/:id" , (req,res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase };
     res.render("urls_show",templateVars)
-})
+});
+app.get("/u/:id", (req, res) => {
+    const id = req.params.id
+    const longUrl = urlDatabase[id]
+    
+    res.redirect(longUrl);
+  });
 app.get("/hello", (req,res) => {
 res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
