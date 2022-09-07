36783a6d9028d17e13c1b00ac0651d350cc6a5b4
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-07 20:42:51 +0000

    committing code

--- express_server.js
+++ express_server.js
@@ -22,6 +22,26 @@ const urlDatabase = {
   
 
 };
+const users = {
+    userRandomID: {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    },
+    user2RandomID: {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk",
+    },
+  };
+const getUserByEmail = (newEmail) => {
+    for(let userId in users){
+        if(users[userId].email.toLowerCase() === newEmail.toLowerCase()){
+        return users[userId];
+        }
+    }
+    return false;
+    };
 app.use(cookieParser());
 app.set("view engine","ejs");
 app.use(express.urlencoded({ extended: true}));
@@ -29,12 +49,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls" , (req,res) => {
-    const templateVars= {urls : urlDatabase, username : req.cookies["name"]};
+    const templateVars= {urls : urlDatabase, users : users[req.cookies["user_id"]]};
     res.render("urls_index", templateVars)
 });
 app.get("/urls/new" , (req,res) => {
-    res.render("urls_new")
-
+    const templateVars= {urls : urlDatabase, users : users[req.cookies["user_id"]]};
+    res.render("urls_new",templateVars)
 });
 app.post("/urls" , (req,res) => {
     const id= Math.random().toString(36).substring(2,8)
@@ -50,7 +70,7 @@ app.post("/urls" , (req,res) => {
 });
 
 app.get("/urls/:id" , (req,res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] , users:users[req.cookies["user_id"]] };
     res.render("urls_show",templateVars)
 });
 app.get("/u/:id", (req, res) => {
@@ -71,16 +91,37 @@ app.post("/urls/:id" , (req,res) => {
     urlDatabase[id] = newUrl;
     res.redirect("/urls");
 })
+app.get("/login", (req,res) => {
+    const templateVars = {users : users[req.cookies["user_id"]]};
+    res.render("logInForm", templateVars)
+})
 app.post("/login" , (req,res) => {
-const {username}=req.body
-console.log(username);
-res.cookie("name", username );
+const { email, password}=req.body
+if(email.length === 0 ){
+    return res.send("400 Status Code")
+};
+if(password.length ===0 ){
+    return res.send("400 Status Code")
+};
+const user = getUserByEmail(email)
+if(!user){
+    return res.send("Staus Code 403. Email cannot be Found");
+}
+if(user){
+    if(password.toLowerCase() !== getUserByEmail(email).password.toLowerCase()){
+        return res.send("Staus Code 403. Password doesn't match")
+    }
+}
+
+console.log(email);
+console.log(password)
+res.cookie("user_id", user.id);
 console.log(req.cookies["name"]);
 res.redirect("/urls")
 })
 app.post("/logout", (req,res) =>{
     
-    res.clearCookie("name");
+    res.clearCookie("user_id");
     res.redirect("/urls");
 })
 app.get("/hello", (req,res) => {
@@ -89,6 +130,37 @@ res.send("<html><body>Hello <b>World</b></body></html>\n");
 app.get("/urls.json",(req,res)=>{
     res.json(urlDatabase)
 });
+
+app.get("/register", (req,res)=>{
+    const templateVars = {users : users[req.cookies["user_id"]]};
+    res.render("registrationPage", templateVars)
+});
+
+app.post("/register", (req,res) => {
+    const user_id= Math.random().toString(36).substring(2,8)
+    const {email, password} = req.body;
+    if(email.length === 0 ){
+        return res.send("400 Status Code")
+    };
+    if(password.length ===0 ){
+        return res.send("400 Status Code")
+    };
+    if(getUserByEmail(email)){
+        return res.send("Staus Code 400. Email already exist");
+    }
+    users[user_id]={
+        id: user_id,
+        email,
+        password,
+    }
+    res.cookie("user_id", user_id);
+    console.log(users);
+    //console.log(password)
+    console.log()
+    res.redirect("/urls");
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
